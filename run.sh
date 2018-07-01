f46cb0d5e43fc0350b2022326faed638dc78c281
T-REX-XP <4560084+T-REX-XP@users.noreply.github.com>
2018-07-01 17:54:19 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,7 +7,6 @@ HVAC_HOST=$(jq --raw-output ".hvac_host" $CONFIG_PATH)
 MQTT_BROKER_URL=$(jq --raw-output ".mqtt.broker_url" $CONFIG_PATH)
 MQTT_TOPIC_PREFIX=$(jq --raw-output ".mqtt.topic_prefix" $CONFIG_PATH)
 
-npm install
 node index.js \
     --hvac-host="${HVAC_HOST}" \
     --mqtt-broker-url="${MQTT_BROKER_URL}" \
